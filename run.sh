1bbc177466ed95b3f38e9ea4281402d64e2daae6
sshleifer <sshleifer@gmail.com>
2015-04-16 18:04:27 -0400

    removed TODO

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# install requirements.text
 if [ "$installed" != "done" ]
 then
   pip install -r requirements.txt
   echo "DONE INSTALLING."
-  echo "IF THERE ARE ERRORS: try $ pip install -r requirements.txt"
+  echo "If problems, try $ pip install -r requirements.txt"
 fi
 export installed="done"
 python chat_to_csv.py
