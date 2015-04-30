5f620429aa9a68ac256149686e52ad7212da37f7
sshleifer <sshleifer@gmail.com>
2015-04-30 15:56:05 -0400

    scatter1.js as resource

--- imsg_stats.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-from chat_to_csv import main
-import webbrowser as wb
-
-main(hidegroups=True)
-print "Serving scatterplot at localhost:8000"
-wb.open("http://localhost:8000")
-call("python -m SimpleHTTPServer 8000",shell=True)
-
