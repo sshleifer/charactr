9bf4d313f63e6407a192e68f3d525d876f61a70b
Peter Dewire <pjdewire@gmail.com>
2015-05-02 03:09:08 -0400

    another df

---
+++ time_chart.py
@@ -0,0 +1,22 @@
+#
+#   A new figure dealing with time
+#
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def timeDf(msg):
+    msg["hour"] = msg.date.apply(lambda x : int(x[-8:-6]))
+    msg_groups = msg.groupby("hour")
+
+    tot = msg_groups.is_sent.size()
+    # first column automatically named 0 for some reason
+    df = pd.DataFrame(tot)
+    df = df.rename(columns={0 : 'tot'})
+    df["snt"] = msg_groups.is_sent.agg(sum)
+    df["rec"] = df.tot - df.snt
+
+    # uncomment to automatically show plots
+    # df.plot()
+    # plt.show()
+    return df
