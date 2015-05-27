98602e2ba4bd5f7b5b0ea51af98368e72f8742e6
sshleifer <sshleifer@gmail.com>
2015-05-27 11:51:15 -0400

    data in d3 friendly format

--- time_chart.py
+++ time_chart.py
@@ -1,14 +1,17 @@
-#New figures dealing with time and date
+#Prepares a DF to be read in for making a timeseries chart,
+#where each line is a contact, the x axis is time,
+#and the y axis is characters exchanged
+#Will eventually be called by chat_to_csv
+
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 def getSumStats(gb):
   '''Number of characters sent and received by each group.'''
   sums = gb.agg(np.sum)
   ret = pd.DataFrame(sums.msg_len)
-  ret['snt'] = sums.snt_chars
-  ret['rec'] = sums.msg_len - sums.snt_chars
+  ret['lensent'] = sums.snt_chars
+  ret['lenrec'] = sums.msg_len - sums.snt_chars
   return ret.reset_index()
 
 def byHour(df, hidegroups=True):
@@ -22,37 +25,37 @@ def byHour(df, hidegroups=True):
   msg_groups = msg.groupby('hour')
   return getSumStats(msg_groups)
   
+#msg.date.apply(lambda x: x[:-9].replace('-','')) for d3 friendly format
+
 def byDate(df, hidegroups=True, byContact=False):
   '''accepts msg dataframe and groups by month.'''
   msg = df.copy()
   if hidegroups: # -hidegroups change
     msg = msg[msg.cname.str.startswith('chat') != True]
-  msg['yr'] = msg.date.apply(lambda x : int(x[:4]))
-  msg['mo'] = msg.date.apply(lambda x : int(x[5:7]))
+  msg['yr'] = msg.date.apply(lambda x : x[:4])
+  msg['mo'] = msg.date.apply(lambda x : x[5:7])
   msg['snt_chars'] = msg['is_sent'] * msg['msg_len']
   groupers = ['yr','mo','cname'] if byContact else ['yr','mo']
   mo_gr = msg.groupby(groupers)
   return getSumStats(mo_gr) 
 
-
-
-def topNFriends(ts, n=10):
+def topN(ts, n=10):
   '''Finds the set of friends that have been in top top N some month'''
-  ts['ym'] = ts.yr.astype('str') + '-' + ts.mo.astype('str')
+  ts['date'] = ts.yr.astype('str') + ts.mo.astype('str') + '01'
   keep = set() 
-  for m in ts.ym.unique():
-    keep.update(ts[ts.ym == m].sort('msg_len',ascending=False)[:10].cname)
+  for m in ts.date.unique():
+    keep.update(ts[ts.date == m].sort('msg_len',ascending=False)[:10].cname)
   return list(keep) 
 
-def bestFriends(msg, n=10):
-  '''returns a dataframe documenting your texting with best n friends
-  over time.'''
+def panelPivot(ts):
+  ret = ts[['date','cname','msg_len']].pivot(index='date',columns='cname', values='msg_len')
+  return ret.fillna(0).reset_index()
+
+def main(msg, n=10):
+  '''returns a DF documenting your texting with best n friends over time.'''
   ts =  byDate(msg, byContact=True)
-  keepers = topNFriends(ts,10)
+  keepers = topN(ts,10)
   ts['keep'] = ts.cname.apply(lambda x: x in keepers)
-  return ts[ts.keep].sort('msg_len', ascending=False)
-
-
-# Cool seaborn plot after calling ` ts=bestFriends(msg)
-#seaborn.pointplot(x='ym',y='msg_len',hue='cname',data=ts)
-    
+  ret = ts[ts.keep].sort('msg_len', ascending=False)
+  #seaborn.pointplot(x='date',y='msg_len',hue='cname',data=ts)
+  return panelPivot(ret)
