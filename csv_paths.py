8c809d3290dfbbbf1e47949c54f35e42b2e03beb
sshleifer <sam@kensho.com>
2016-05-07 13:39:47 -0400

    two spaces to  spaces

---
+++ csv_paths.py
@@ -0,0 +1,5 @@
+# relative to project root
+
+MSG_PATH = 'outputs/msg.csv'
+TS_PATH = 'outputs/ts.csv'
+PANEL_PATH = 'outputs/ppl.csv'
--- imsg_stats.py
+++ imsg_stats.py
@@ -1,8 +1,6 @@
 from subprocess import call
-from chat_to_csv import main
 import webbrowser as wb
 
-# main(hidegroups=True)
 print "Serving scatterplot at localhost:8000"
 wb.open("http://localhost:8014")
-call("python -m SimpleHTTPServer 8014",shell=True)
+call("python -m SimpleHTTPServer 8014", shell=True)
