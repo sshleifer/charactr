830473ff89baef96a5c4401820a4a11e00ecdd6e
sshleifer <sam@kensho.com>
2018-01-15 10:41:37 -0500

    More deletion

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mv msg.csv data
-mv ppl.csv data
-mv ts.csv data
-python imsg_stats.py
--- transcript.js
+++
@@ -1,9 +0,0 @@
-var transcript = function() {
-    var target = document.getElementById("targetLocation");
-
-    target.appendChild(document.createTextNode("HELLO"));
-}
-
-var clearBox = function(elementID) {
-    document.getElementById(elementID).innerHTML = "";
-}
